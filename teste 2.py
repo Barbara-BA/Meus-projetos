b457d99ee649054dd9ab6049290e3b3a39c70641
Barbara-BA <143759992+Barbara-BA@users.noreply.github.com>
2023-11-03 20:13:20 -0300

    Add files via upload

---
+++ teste 2.py
@@ -0,0 +1,10 @@
+nome= input ('Qual é o seu nome ?')
+mensagem= print ('Olá', nome, '! Seja bem vindo!')
+
+mensagem= print ('Qual é a sua data de nascimento?')
+
+dia= input ('dia')
+mes= input ('mês')
+ano= input ('ano')
+mensagem= print ('você nasceu no dia', dia, 'de', mes, 'de', ano, 'correto?')
+
---
+++ teste 3.py
@@ -0,0 +1,4 @@
+Somatoria= print ('Qual a somatoria de dois números que o resultado seja igual a 8?')
+primeironumero= input ('primeiro número')
+segundonumero= input ('segundo número')
+soma= print (, 
